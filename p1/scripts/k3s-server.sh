45bc19771d3be45817ea3c43faf8ca055a6598ad
tsuchen <tsuchen@IOT.myguest.virtualbox.org>
2025-03-19 18:42:47 +0100

    Changed box to bento/debian, works well in school DELL computers

--- p1/scripts/k3s-server.sh
+++ p1/scripts/k3s-server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-sudo apt-get update
 sudo curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --write-kubeconfig-mode=644 --node-ip $1 --bind-address=$1" sh -s -
-sleep 15
+sleep 10
 cp /var/lib/rancher/k3s/server/node-token /vagrant/.
